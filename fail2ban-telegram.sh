e1a2698732a09e1e69413110809ec3477be04886
Jaime <jayepesviloria@gmail.com>
2021-02-27 15:37:09 -0500

    refactor: Validado por shellcheck

--- fail2ban-telegram.sh
+++ fail2ban-telegram.sh
@@ -10,6 +10,9 @@
 # unban
 # Optional second argument: Ip for ban/unband
 
+servername=$(hostname)
+apiToken="input apiToken"
+chatId="input chat id"
 
 # Display usage information
 function show_usage {
@@ -23,11 +26,9 @@ function show_usage {
 # Send notification
 function send_msg {
   msg=$@
-  apiToken=<input bot token>
-  chatId=<input chat id>
   url="https://api.telegram.org/bot$apiToken/sendMessage"
 
-  curl -s -X POST $url -d chat_id=$chatId -d "text=$msg" -d parse_mode="html"
+  curl -s -X POST "$url" -d chat_id="$chatId" -d text="$msg" -d parse_mode="html"
   exit
 }
 
@@ -39,31 +40,30 @@ then
 fi
 
 geoip_url="https://get.geojs.io/v1/ip/country/full/$2"
-servername=<input server name>
 # Take action depending on argument
 if [ "$1" = 'start' ]
 then
   msg="$servername sheild on"
-  send_msg $msg
+  send_msg "$msg"
 elif [ "$1" = 'stop' ]
 then
   msg="$servername server shield turning off"
-  send_msg $msg
+  send_msg "$msg"
 elif [ "$1" = 'ban' ]
 then
   #url="https://get.geojs.io/v1/ip/country/full/$2"
-  country=$(curl -s $geoip_url)
+  country=$(curl -s "$geoip_url")
   full="$servername server shield banned %0AIP: <code>$2</code> %0ACountry: <code>$country</code>"
   half="$servername server shield banned an ip."
-  msg=$([ "$2" != '' ] && echo -e $full || echo -e $half )
-  send_msg $msg
+  msg=$([ "$2" != '' ] && echo -e "$full" || echo -e "$half" )
+  send_msg "$msg"
 elif [ "$1" = 'unban' ]
 then
-  country=$(curl -s $geoip_url)
+  country=$(curl -s "$geoip_url")
   full="$servername server shield unban %0AIP: <code>$2</code> %0ACountry: <code>$country</code>"
   half="$servername server shield unban an ip."
-  msg=$([ "$2" != '' ] && echo -e $full || echo -e $half )
-  send_msg $msg
+  msg=$([ "$2" != '' ] && echo -e "$full" || echo -e "$half" )
+  send_msg "$msg"
 else
   show_usage
 fi
